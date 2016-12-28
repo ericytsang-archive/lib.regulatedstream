c2aa9888012b7709cf9b378b2e0fd91fc7dd6539
Eric Tsang <surplus.et@gmail.com>
2016-12-28 03:51:36 -0800

    added regulated stream class
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

---
+++ src/main/kotlin/com/ericytsang/github/lib/regulatedstream/RegulatedStream.kt
@@ -0,0 +1,47 @@
+package com.ericytsang.github.lib.regulatedstream
+
+import com.github.ericytsang.lib.abstractstream.AbstractOutputStream
+import java.io.OutputStream
+import java.util.concurrent.LinkedBlockingQueue
+
+class RegulatedOutputStream(val stream:OutputStream):AbstractOutputStream()
+{
+    private val permits = LinkedBlockingQueue<Int>()
+
+    private var currentPermit = 0
+
+    /**
+     * allow the output stream to write [numBytes] more bytes in addition to
+     * previous calls to permit.
+     */
+    fun permit(numBytes:Int)
+    {
+        if (numBytes < 0)
+        {
+            throw IllegalArgumentException("argument must be > 0: $numBytes")
+        }
+        permits.put(numBytes)
+    }
+
+    override fun doClose() = stream.close()
+    override fun flush() = stream.flush()
+
+    override fun doWrite(b:ByteArray,off:Int,len:Int)
+    {
+        var cursor = off
+        var remainingLen = len
+        while (remainingLen > 0)
+        {
+            while (currentPermit == 0)
+            {
+                currentPermit = permits.take()
+            }
+            val bytesToTransmit = Math.min(currentPermit,remainingLen)
+            stream.write(b,cursor,bytesToTransmit)
+            currentPermit -= bytesToTransmit
+            remainingLen -= bytesToTransmit
+            cursor += bytesToTransmit
+        }
+    }
+}
+
