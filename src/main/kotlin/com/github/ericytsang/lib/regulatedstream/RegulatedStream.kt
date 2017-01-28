4bfe8543aa6fda3b2d211c2662e0c8a207898a63
Eric Tsang <surplus.et@gmail.com>
2017-01-28 14:27:07 -0800

    possible to see how many permits are left
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- src/main/kotlin/com/github/ericytsang/lib/regulatedstream/RegulatedStream.kt
+++ src/main/kotlin/com/github/ericytsang/lib/regulatedstream/RegulatedStream.kt
@@ -2,25 +2,38 @@ package com.github.ericytsang.lib.regulatedstream
 
 import com.github.ericytsang.lib.abstractstream.AbstractOutputStream
 import java.io.OutputStream
-import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 class RegulatedOutputStream(val stream:OutputStream):AbstractOutputStream()
 {
-    private val permits = LinkedBlockingQueue<Int>()
+    private val permitsMutex = ReentrantLock()
 
-    private var currentPermit = 0
+    private val permitsChanged = permitsMutex.newCondition()
+
+    var permits = 0
+        private set(value)
+        {
+            check(permitsMutex.isHeldByCurrentThread)
+            field = value
+            permitsChanged.signalAll()
+        }
 
     /**
      * allow the output stream to write [numBytes] more bytes in addition to
      * previous calls to permit.
      */
-    fun permit(numBytes:Int)
+    fun permit(numBytes:Int) = permitsMutex.withLock()
     {
         if (numBytes < 0)
         {
             throw IllegalArgumentException("argument must be > 0: $numBytes")
         }
-        permits.put(numBytes)
+        while (Int.MAX_VALUE-permits < numBytes)
+        {
+            permitsChanged.await()
+        }
+        permits += numBytes
     }
 
     override fun doClose()
@@ -31,22 +44,21 @@ class RegulatedOutputStream(val stream:OutputStream):AbstractOutputStream()
 
     override fun flush() = stream.flush()
 
-    override fun doWrite(b:ByteArray,off:Int,len:Int)
+    override fun doWrite(b:ByteArray,off:Int,len:Int) = permitsMutex.withLock()
     {
         var cursor = off
         var remainingLen = len
         while (remainingLen > 0)
         {
-            while (currentPermit == 0)
+            while (permits <= 0)
             {
-                currentPermit = permits.take()
+                permitsChanged.await()
             }
-            val bytesToTransmit = Math.min(currentPermit,remainingLen)
+            val bytesToTransmit = Math.min(permits,remainingLen)
             stream.write(b,cursor,bytesToTransmit)
-            currentPermit -= bytesToTransmit
+            permits -= bytesToTransmit
             remainingLen -= bytesToTransmit
             cursor += bytesToTransmit
         }
     }
 }
-
